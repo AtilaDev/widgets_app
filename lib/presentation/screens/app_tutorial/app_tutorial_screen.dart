74b9ced689f338735f62a190b8692265476c4c01
Leandro <atiladevelop@gmail.com>
2023-05-01 10:55:22 -0300

    pageview finalizado

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,12 +1,151 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      'Busca la comida',
+      'Sint excepteur pariatur non irure amet reprehenderit occaecat esse minim tempor reprehenderit cupidatat laboris magna. Velit esse commodo reprehenderit enim adipisicing nostrud do sint eu pariatur sunt. Culpa id duis amet exercitation.',
+      'assets/images/1.png'),
+  SlideInfo(
+      'Entrega rápida',
+      'Minim nulla eu sunt aliquip. Veniam ea ex qui consequat quis. Culpa quis tempor eiusmod consectetur. Veniam dolore aute veniam sunt labore. Ad esse laborum pariatur ut laborum adipisicing ullamco aliquip exercitation sit sint dolor proident eu.',
+      'assets/images/2.png'),
+  SlideInfo(
+      'Disfruta la comida',
+      'Duis cillum dolore elit enim est esse nisi elit veniam enim id excepteur. Tempor cillum duis labore pariatur eu Lorem adipisicing. Ullamco commodo ullamco sit excepteur laboris occaecat culpa ex commodo mollit nostrud. Ut quis do voluptate esse aliquip aliquip.',
+      'assets/images/3.png'),
+];
+
+class AppTutorialScreen extends StatefulWidget {
   static const name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageViewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageViewController.addListener(() {
+      final page = pageViewController.page ?? 0;
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: TextButton(
+              child: const Text('Salir'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                    from: 15,
+                    duration: const Duration(seconds: 1),
+                    child: FilledButton(
+                      onPressed: () => context.pop(),
+                      child: const Text('Comenzar'),
+                    ),
+                  ),
+                )
+              : const SizedBox()
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final myThemeContext = Theme.of(context).textTheme;
+    final titleStyle = myThemeContext.titleLarge;
+    final captionStyle = myThemeContext.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(
+            image: AssetImage(imageUrl),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            title,
+            style: titleStyle,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            caption,
+            style: captionStyle,
+          )
+        ],
+      ),
+    );
   }
 }
